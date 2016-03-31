b7e2ec91b78a3cf4cf03635ecdbfa079c35abbf8
Christine Du <duyuqingecho@gmail.com>
2016-03-31 19:36:46 -0400

    max and min finished

--- student_func.cu
+++ student_func.cu
@@ -1,86 +1,103 @@
-/* Udacity Homework 3
-   HDR Tone-mapping
-
-  Background HDR
-  ==============
-
-  A High Dynamic Range (HDR) image contains a wider variation of intensity
-  and color than is allowed by the RGB format with 1 byte per channel that we
-  have used in the previous assignment.  
-
-  To store this extra information we use single precision floating point for
-  each channel.  This allows for an extremely wide range of intensity values.
-
-  In the image for this assignment, the inside of church with light coming in
-  through stained glass windows, the raw input floating point values for the
-  channels range from 0 to 275.  But the mean is .41 and 98% of the values are
-  less than 3!  This means that certain areas (the windows) are extremely bright
-  compared to everywhere else.  If we linearly map this [0-275] range into the
-  [0-255] range that we have been using then most values will be mapped to zero!
-  The only thing we will be able to see are the very brightest areas - the
-  windows - everything else will appear pitch black.
-
-  The problem is that although we have cameras capable of recording the wide
-  range of intensity that exists in the real world our monitors are not capable
-  of displaying them.  Our eyes are also quite capable of observing a much wider
-  range of intensities than our image formats / monitors are capable of
-  displaying.
-
-  Tone-mapping is a process that transforms the intensities in the image so that
-  the brightest values aren't nearly so far away from the mean.  That way when
-  we transform the values into [0-255] we can actually see the entire image.
-  There are many ways to perform this process and it is as much an art as a
-  science - there is no single "right" answer.  In this homework we will
-  implement one possible technique.
-
-  Background Chrominance-Luminance
-  ================================
-
-  The RGB space that we have been using to represent images can be thought of as
-  one possible set of axes spanning a three dimensional space of color.  We
-  sometimes choose other axes to represent this space because they make certain
-  operations more convenient.
-
-  Another possible way of representing a color image is to separate the color
-  information (chromaticity) from the brightness information.  There are
-  multiple different methods for doing this - a common one during the analog
-  television days was known as Chrominance-Luminance or YUV.
-
-  We choose to represent the image in this way so that we can remap only the
-  intensity channel and then recombine the new intensity values with the color
-  information to form the final image.
-
-  Old TV signals used to be transmitted in this way so that black & white
-  televisions could display the luminance channel while color televisions would
-  display all three of the channels.
-  
-
-  Tone-mapping
-  ============
-
-  In this assignment we are going to transform the luminance channel (actually
-  the log of the luminance, but this is unimportant for the parts of the
-  algorithm that you will be implementing) by compressing its range to [0, 1].
-  To do this we need the cumulative distribution of the luminance values.
-
-  Example
-  -------
-
-  input : [2 4 3 3 1 7 4 5 7 0 9 4 3 2]
-  min / max / range: 0 / 9 / 9
-
-  histo with 3 bins: [4 7 3]
-
-  cdf : [4 11 14]
-
-
-  Your task is to calculate this cumulative distribution by following these
-  steps.
-
-*/
-
 #include "utils.h"
 
+#define tbp 512
+
+// kernel for calculating minimum value
+__global__ void cmin(float *d_in, float *min, int len)
+{
+  extern __shared__ float smin[]; 
+
+  unsigned int tid = threadIdx.x;
+  unsigned int i = blockIdx.x * blockDim.x  + threadIdx.x;
+
+  smin[tid] = d_in[i]<d_in[i+len] ? d_in[i] : d_in[i+len];
+
+  __syncthreads();
+  if(blockDim.x > 256 && tid<256) {if(smin[tid] > smin[tid+256]) smin[tid] = smin[tid+256];}  __syncthreads();
+  if(blockDim.x > 128 && tid<128) {if(smin[tid] > smin[tid+128]) smin[tid] = smin[tid+128];}  __syncthreads();
+  if(blockDim.x > 64 && tid<64) {if(smin[tid] > smin[tid+64]) smin[tid] = smin[tid+64];}  __syncthreads();
+  if(tid<32) {
+    if(blockDim.x > 32 && smin[tid] > smin[tid+32]) smin[tid] = smin[tid+32];
+    if(blockDim.x > 16 && smin[tid] > smin[tid+16]) smin[tid] = smin[tid+16];
+    if(blockDim.x > 8 && smin[tid] > smin[tid+8]) smin[tid] = smin[tid+8];
+    if(blockDim.x > 4 && smin[tid] > smin[tid+4]) smin[tid] = smin[tid+4];
+    if(blockDim.x > 2 && smin[tid] > smin[tid+2]) smin[tid] = smin[tid+2];
+    if(smin[tid] > smin[tid+1]) smin[tid] = smin[tid+1];
+    __syncthreads();
+    }  
+
+  if(tid == 0 ) 
+  {
+    min[blockIdx.x] = smin[0];
+  }
+}
+__global__ void cmax(float *d_in, float *max, int len)
+{
+  extern __shared__ float smax[]; 
+
+  unsigned int tid = threadIdx.x;
+  unsigned int i = blockIdx.x * blockDim.x  + threadIdx.x;
+
+  smax[tid] = d_in[i]>d_in[i+len] ? d_in[i] : d_in[i+len];
+
+  __syncthreads();
+
+  if(blockDim.x > 256 && tid<256) {if(smax[tid] < smax[tid+256]) smax[tid] = smax[tid+256];}  __syncthreads();
+  if(blockDim.x > 128 && tid<128) {if(smax[tid] < smax[tid+128]) smax[tid] = smax[tid+128];}  __syncthreads();
+  if(blockDim.x > 64 && tid<64) {if(smax[tid] < smax[tid+64]) smax[tid] = smax[tid+64];}  __syncthreads();
+  if(tid<32) {
+    if(blockDim.x > 32 && smax[tid] < smax[tid+32]) smax[tid] = smax[tid+32];
+    if(blockDim.x > 16 && smax[tid] < smax[tid+16]) smax[tid] = smax[tid+16];
+    if(blockDim.x > 8 && smax[tid] < smax[tid+8]) smax[tid] = smax[tid+8];
+    if(blockDim.x > 4 && smax[tid] < smax[tid+4]) smax[tid] = smax[tid+4];
+    if(blockDim.x > 2 && smax[tid] < smax[tid+2]) smax[tid] = smax[tid+2];
+    if(smax[tid] < smax[tid+1]) smax[tid] = smax[tid+1];
+    __syncthreads();
+    } 
+  if(tid == 0 ) 
+  {
+    max[blockIdx.x] = smax[0];
+  }
+}
+
+float get_min_max(const float* const d_in, const size_t size, int flag){
+  float result;
+
+  float *dev_in, *dev_out;
+  cudaMalloc((void **) &dev_in, size * sizeof(float));
+  cudaMemcpy(dev_in, d_in, size * sizeof(float), cudaMemcpyHostToDevice);
+
+  int nblocks = ( (size + tbp - 1) / tbp) /2;
+  int nblocks_pre = nblocks;
+
+  while(nblocks>1){
+    cudaMalloc((void **) &dev_out, nblocks * sizeof(float));
+    if (flag){
+      cmin <<<nblocks,tbp,tbp * sizeof(float)>>>(dev_in, dev_out, tbp); 
+    }else {
+      cmax <<<nblocks,tbp,tbp * sizeof(float)>>>(dev_in, dev_out, tbp); 
+    }
+    
+    cudaFree(dev_in);
+    dev_in = dev_out;
+    nblocks_pre = nblocks;
+    nblocks =( (nblocks + tbp - 1) / tbp) /2;
+
+  }
+  cudaMalloc((void **) &dev_out, sizeof(float));
+  if(flag){
+    cmin <<<1,nblocks_pre/2,nblocks_pre * sizeof(float)/2>>>(dev_in, dev_out, nblocks_pre/2); 
+  }else{
+    cmax <<<1,nblocks_pre/2,nblocks_pre * sizeof(float)/2>>>(dev_in, dev_out, nblocks_pre/2); 
+  }
+  
+  
+  cudaMemcpy(&result, dev_out, sizeof(float),cudaMemcpyDeviceToHost);
+  
+  cudaFree(dev_out);
+  cudaFree(dev_in);
+  return result;
+}
 void your_histogram_and_prefixsum(const float* const d_logLuminance,
                                   unsigned int* const d_cdf,
                                   float &min_logLum,
@@ -99,6 +116,14 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
     4) Perform an exclusive scan (prefix sum) on the histogram to get
        the cumulative distribution of luminance values (this should go in the
        incoming d_cdf pointer which already has been allocated for you)       */
+
+    const size_t size = numRows*numCols;
+    min_logLum = get_min_max(d_logLuminance, size, 1);
+    max_logLum = get_min_max(d_logLuminance, size, 0);
+    
+    printf("got min of %f\n", min_logLum);
+    printf("got max of %f\n", max_logLum);
+    printf("numBins %d\n", numBins);
 
 
 }
